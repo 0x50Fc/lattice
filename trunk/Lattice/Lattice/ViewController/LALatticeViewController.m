469c9f8f07aca2427c9369099f061dadd0c93c35
flyingbird <wsdhr1982@gmail.com>
2014-01-11 13:21:46 +0800

    update

--- trunk/Lattice/Lattice/ViewController/LALatticeViewController.m
+++ trunk/Lattice/Lattice/ViewController/LALatticeViewController.m
@@ -12,6 +12,9 @@
 
 @interface LALatticeViewController ()
 @property(nonatomic,retain) LALatticeBeaconQueryTask * beaconQueryTask;
+@property(nonatomic,assign) BOOL bShowingBeacon;
+@property(nonatomic,retain) UIView *tipView;
+@property(nonatomic,retain) NSDictionary *currentDict;
 @end
 
 @implementation LALatticeViewController
@@ -82,6 +85,7 @@
     LALatticeBeaconTask * beaconTask = [[LALatticeBeaconTask alloc] init];
     [beaconTask setBeaconKey:[(id<LAContext>)self.context beaconKey]];
     [beaconTask setInfoObject:_dataObject.infoObject];
+    [beaconTask setUrl:_dataObject.url];
     [self.context handle:@protocol(ILALatticeBeaconTask) task:beaconTask priority:0];
 }
 
@@ -94,12 +98,101 @@
 }
 
 -(void) vtUploadTask:(id<IVTUplinkTask>) uplinkTask didSuccessResults:(id) results forTaskType:(Protocol *) taskType {
-    self.infoObjects = [(id<ILALatticeBeaconQueryTask>)uplinkTask infoObjects];
-    [(id<ILALatticeBeaconQueryTask>)uplinkTask setDelegate:nil];
+    if(taskType == @protocol(ILALatticeBeaconQueryTask) || taskType == @protocol(ILALatticeBeaconTask)){
+        self.infoObjects = [(id<ILALatticeBeaconQueryTask>)uplinkTask infoObjects];
+        [(id<ILALatticeBeaconQueryTask>)uplinkTask setDelegate:nil];
+        if (!_bShowingBeacon)
+            [self presentBeaconBehavior:0];
+    }
+    else{
+        [super vtUploadTask:uplinkTask didSuccessResults:results forTaskType:taskType];
+    }
 }
 
 -(void) vtUploadTask:(id<IVTUplinkTask>) uplinkTask didFailWithError:(NSError *)error forTaskType:(Protocol *)taskType {
-    [(id<ILALatticeBeaconQueryTask>)uplinkTask setDelegate:nil];
+    if(taskType == @protocol(ILALatticeBeaconQueryTask) || taskType == @protocol(ILALatticeBeaconTask)){
+        [(id<ILALatticeBeaconQueryTask>)uplinkTask setDelegate:nil];
+    }
+    else{
+        [super vtUploadTask:uplinkTask didFailWithError:error forTaskType:taskType];
+    }
+}
+
+-(void)presentBeaconBehavior:(NSInteger)index {
+    _bShowingBeacon = YES;
+    NSDictionary *dict = nil;
+    @synchronized(self) {
+        if (index < _infoObjects.count) {
+            dict = [[_infoObjects allValues] objectAtIndex:index];
+            self.currentDict = dict;
+            if ([dict stringValueForKey:@"url"] == nil) {
+                dict = nil;
+                self.currentDict = nil;
+            }
+            else {
+                if ([_dataObject.url isEqualToString:[dict stringValueForKey:@"url"]]) {
+                    dict = nil;
+                    self.currentDict = nil;
+                }
+            }
+        }
+    }
+    if (dict) {
+        BOOL bDoNotDismiss = (index == _infoObjects.count-1);
+        self.tipView = [[UIView alloc] initWithFrame:CGRectMake(0, -44, self.view.frame.size.width, 44)];
+        _tipView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.8];
+        UIButton *tipButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        tipButton.frame = _tipView.bounds;
+        [tipButton setTitle:[NSString stringWithFormat:@"您身边的人正在阅读:%@,点击查看",[dict stringValueForKeyPath:@"infoObject.title"]] forState:UIControlStateNormal];
+        [tipButton.titleLabel setFont:[UIFont systemFontOfSize:15]];
+        [tipButton.titleLabel setTextAlignment:NSTextAlignmentCenter];
+        [tipButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [tipButton addTarget:self action:@selector(tipClick:) forControlEvents:UIControlEventTouchUpInside];
+        [_tipView addSubview:tipButton];
+        [self.view addSubview:_tipView];
+        
+//        NSLog(@"AddsubView");
+        
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+        [UIView setAnimationDuration:1.0];
+        [_tipView setFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
+        [UIView commitAnimations];
+        
+        if (!bDoNotDismiss) {
+            __weak UIView * refView = _tipView;
+            dispatch_time_t delayTime = dispatch_time(DISPATCH_TIME_NOW, 5ull * NSEC_PER_SEC);
+            dispatch_after(delayTime,dispatch_get_main_queue(),^(void){
+                [UIView animateWithDuration:1 animations:^(void){
+                [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+                    [refView setFrame:CGRectMake(0, -44, self.view.frame.size.width, 44)];
+                } completion:^(BOOL finished) {
+                    [refView removeFromSuperview];
+                    
+                    dispatch_time_t delayTime = dispatch_time(DISPATCH_TIME_NOW, 1ull * NSEC_PER_SEC);
+                    dispatch_after(delayTime,dispatch_get_main_queue(),^(void){
+                        [self presentBeaconBehavior:index+1];
+                    });
+                }];
+            });
+        }
+        else {
+            _bShowingBeacon = NO;
+        }
+    }
+    else {
+        [UIView animateWithDuration:1 animations:^(void){
+            [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+            [_tipView setFrame:CGRectMake(0, -44, self.view.frame.size.width, 44)];
+        } completion:^(BOOL finished) {
+            [_tipView removeFromSuperview];
+        }];
+        _bShowingBeacon = NO;
+    }
+}
+
+-(void)tipClick:(id)sender {
+    [self openLatticeUrl:[_currentDict stringValueForKey:@"url"]];
 }
 
 @end
